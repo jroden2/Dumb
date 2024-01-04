672d591fff6e065e5a75b11215eca3270a86d5d1
Jack <95051977+jroden2@users.noreply.github.com>
2024-01-04 15:38:02 +0000

    Create Undownload.go

---
+++ Undownload.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+	const col = 30
+	bar := fmt.Sprintf("\x0c[%%-%vs]", col)
+	for i := 0; i < col; i++ {
+		fmt.Printf(bar+" %s", strings.Repeat("=", i)+">", "Downloading...")
+		time.Sleep(100 * time.Millisecond)
+	}
+  // [==============================] Downloaded!
+	fmt.Printf(bar+" Downloaded!", strings.Repeat("=", col))
+
+
+  // Prints an '[ERROR                         ] Downloaded!' message
+	fmt.Printf(bar+" Downloaded!", "ERROR")
+	time.Sleep(500 * time.Millisecond)
+
+  // Wipes the bar [==============================>] Downloaded!
+	for i := col; i > 0; i-- {
+		fmt.Printf(bar+" %s", strings.Repeat("=", i)+">", "Undownloading...")
+		time.Sleep(100 * time.Millisecond)
+	}
+  // [                              ] Undownloaded!
+	fmt.Printf(bar+" Undownloaded!", strings.Repeat(" ", col))
+}
