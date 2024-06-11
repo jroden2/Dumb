5adee13d7ef610ea3e47dbb50b1414d6614c4ca5
Jack <95051977+jroden2@users.noreply.github.com>
2024-06-11 16:14:12 +0100

    Create Text.go

---
+++ Text.go
@@ -0,0 +1,21 @@
+package github.com/jroden2/dumb
+
+func ToCamelCase(input string) {
+	uppercase := false
+	output := ""
+
+	for _, x := range input {
+		x := string(x)
+		if uppercase {
+			x = strings.ToUpper(x)
+			uppercase = false
+		} else {
+			x = strings.ToLower(x)
+			uppercase = true
+		}
+
+		output = output + x
+	}
+
+	return output
+}
